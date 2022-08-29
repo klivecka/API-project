a39bee86aed1a9335b794d4d4b26a8bdc55bf621
Klivecka <alexanderklivecka@ALEXANDERs-MacBook-Pro.local>
2022-08-29 13:25:38 -0600

    login-validation-test-success

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -3,10 +3,21 @@ const { setTokenCookie, restoreUser } = require("../../utils/auth");
 const { User } = require("../../db/models");
 const router = express.Router();
 
-
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
+const validateLogin = [
+  check('credential')
+    .exists({ checkFalsy: true })
+    .notEmpty()
+    .withMessage('Please provide a valid email or username.'),
+  check('password')
+    .exists({ checkFalsy: true })
+    .withMessage('Please provide a password.'),
+  handleValidationErrors
+];
 
 //SIGN IN ROUTE
-router.post("/", async (req, res, next) => {
+router.post("/", validateLogin, async (req, res, next) => {
     const { credential, password } = req.body;
 
     const user = await User.login({ credential, password });
@@ -47,6 +58,8 @@ router.delete(
       } else return res.json({});
     }
   );
+
+//VALIDATION FOR THE REQUEST BODY
 
 
 
