8bc503baf2ea9914f6c69c69a89ce8b1d3071250
Klivecka <alexanderklivecka@ALEXANDERs-MacBook-Pro.local>
2022-08-30 10:48:00 -0600

    groupseed2

--- backend/db/seeders/20220830163349-groupseed.js
+++ backend/db/seeders/20220830163349-groupseed.js
@@ -1,42 +1,64 @@
-'use strict';
+"use strict";
 
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    return queryInterface.bulkInsert(
-      "Groups",
-      [
-          {
-          name: 
-          organizerId: 
-          about: 
-          type: 
-          private: 
-          city: 
-          state: 
-          numMembers:
-          previewImage: 
-          },
-      ],
-      {}
-  );
-   
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
-  },
+    async up(queryInterface, Sequelize) {
+        return queryInterface.bulkInsert(
+            "Groups",
+            [
+                {
+                    name: "Guitar Hero Meetup",
+                    organizerId: 3,
+                    about: "lets hang out and play guitar hero my guys! awesome fun times or everyone and free soda",
+                    type: "In person",
+                    private: true,
+                    city: "Salt Lake City",
+                    state: "UT",
+                    numMembers: 10,
+                    previewImage: "../assets/Guitar-hero-iii-cover-image.jpeg",
+                },
+                {
+                    name: "Lets play some soccer",
+                    organizerId: 1,
+                    about: "this is an awesome soccer meetup we meet on the field by the elementary school on wednesdays",
+                    type: "In person",
+                    private: false,
+                    city: "Cottonwood Heights",
+                    state: "UT",
+                    numMembers: 30,
+                    previewImage: "../assets/Football.jpeg",
+                },
+                {
+                    name: "online d and d meetup",
+                    organizerId: 2,
+                    about: "this is an online group that meets to play dungeions and dragons on zoom. its really fun come hang with us fellow nerds,",
+                    type: "Online",
+                    private: true,
+                    city: "Any City",
+                    state: "US",
+                    numMembers: 100,
+                    previewImage: "../assets/dice.jpeg",
+                },
+            ],
+            {}
+        );
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
-  }
+        /**
+         * Add seed commands here.
+         *
+         * Example:
+         * await queryInterface.bulkInsert('People', [{
+         *   name: 'John Doe',
+         *   isBetaMember: false
+         * }], {});
+         */
+    },
+
+    async down(queryInterface, Sequelize) {
+        /**
+         * Add commands to revert seed here.
+         *
+         * Example:
+         * await queryInterface.bulkDelete('People', null, {});
+         */
+    },
 };
