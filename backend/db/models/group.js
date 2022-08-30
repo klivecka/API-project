85955738827a01571a68d9a28772dd1825c7b67a
Klivecka <alexanderklivecka@ALEXANDERs-MacBook-Pro.local>
2022-08-30 09:53:39 -0600

    group model2

--- backend/db/models/group.js
+++ backend/db/models/group.js
@@ -1,50 +1,73 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
-  class Group extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
+    class Group extends Model {
+        /**
+         * Helper method for defining associations.
+         * This method is not a part of Sequelize lifecycle.
+         * The `models/index` file will call this method automatically.
+         */
+        static associate(models) {
+            // define association here
+        }
     }
-  }
-  Group.init({
-    name: {
-      type: DataTypes.STRING,
-    },
-    organizerId: {
-      type: DataTypes.INTEGER,
-    },
-    about: {
-      type: DataTypes.STRING
-    },
-    groupType: {
-      type: DataTypes.STRING
-    },
-    isPrivate: {
-      type: DataTypes.BOOLEAN,
-    },
-    city: {
-      type: DataTypes.STRING,
-    },
-    state: {
-      type: DataTypes.STRING,
-    },
-    numMembers: {
-      type: DataTypes.INTEGER,
-    },
-    previewImage: {
-      type: DataTypes.STRING,
-    }
-
-  }, {
-    sequelize,
-    modelName: 'Group',
-  });
-  return Group;
+    Group.init(
+        {
+            name: {
+                type: DataTypes.STRING,
+                validate: {
+                    len: [0, 60],
+                },
+            },
+            organizerId: {
+                type: DataTypes.INTEGER,
+            },
+            about: {
+                type: DataTypes.STRING,
+                validate: {
+                    len: [50, 5000],
+                },
+            },
+            type: {
+                type: DataTypes.STRING,
+                validate: {
+                    validType() {
+                        if (
+                            this.type !== "Online" ||
+                            this.type !== "In person"
+                        ) {
+                            throw new Error(
+                                "Type must be 'Online' or 'In person'"
+                            );
+                        }
+                    },
+                },
+            },
+            isPrivate: {
+                type: DataTypes.BOOLEAN,
+            },
+            city: {
+                type: DataTypes.STRING,
+                allowNull: false,
+            },
+            state: {
+                type: DataTypes.STRING,
+                allowNull: false,
+                validate: {
+                  len: [2, 2],
+                }
+            },
+            numMembers: {
+                type: DataTypes.INTEGER,
+            },
+            previewImage: {
+                type: DataTypes.STRING,
+            },
+        },
+        {
+            sequelize,
+            modelName: "Group",
+        }
+    );
+    return Group;
 };
