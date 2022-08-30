8329a0a3601e5a9b4d8236adb0488d48330d81e1
Klivecka <alexanderklivecka@ALEXANDERs-MacBook-Pro.local>
2022-08-30 09:54:59 -0600

    group model3

--- backend/db/models/group.js
+++ backend/db/models/group.js
@@ -43,7 +43,7 @@ module.exports = (sequelize, DataTypes) => {
                     },
                 },
             },
-            isPrivate: {
+            private: {
                 type: DataTypes.BOOLEAN,
             },
             city: {
