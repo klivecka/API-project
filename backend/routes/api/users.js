db045b2f7b0b428089f7043fb1ed902c59e9bb11
Klivecka <alexanderklivecka@ALEXANDERs-MacBook-Pro.local>
2022-09-01 19:37:02 -0600

    user sign up error handler

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const { setTokenCookie, requireAuth } = require("../../utils/auth");
 const { User } = require("../../db/models");
-const { check } = require("express-validator");
+const { body, validationResult, check } = require("express-validator");
 const { handleValidationErrors } = require("../../utils/validation");
 
 const router = express.Router();
@@ -10,42 +10,37 @@ const validateSignup = [
     check("email")
         .exists({ checkFalsy: true })
         .isEmail()
-        .withMessage("Please provide a valid email."),
-    check("username")
+        .withMessage("Invalid email"),
+    check("firstName")
         .exists({ checkFalsy: true })
-        .isLength({ min: 4 })
-        .withMessage("Please provide a username with at least 4 characters."),
-    check("username")
-        .not()
-        .isEmail()
-        .withMessage("Username cannot be an email."),
-    check("password")
+        .withMessage("First Name is required"),
+    check("lastName")
         .exists({ checkFalsy: true })
-        .isLength({ min: 6 })
-        .withMessage("Password must be 6 characters or more."),
+        .withMessage("Last Name is required"),
     handleValidationErrors,
 ];
 
 router.post("/", validateSignup, async (req, res) => {
     const { firstName, lastName, email, username, password } = req.body;
     const userCheck = await User.findOne({
-      where: {
-        email: email
-      }
-    })
+        where: {
+            email: email,
+        },
+    });
 
-
-    
     if (userCheck) {
-      res.status(403)
-      res.json({
-        "message": "User already exists",
-        "statusCode": 403,
-        "errors": {
-          "email": "User with that email already exists"
-        }
-      })
+        res.status(403);
+        res.json({
+            message: "User already exists",
+            statusCode: 403,
+            errors: {
+                email: "User with that email already exists",
+            },
+        });
     }
+
+
+
     const user = await User.signup({
         firstName,
         lastName,
@@ -61,7 +56,7 @@ router.post("/", validateSignup, async (req, res) => {
         },
     });
     const userObj = userRes.toJSON();
-    userObj.token = token
+    userObj.token = token;
 
     // const userObj = userRes.userRes
     console.log("\n");
