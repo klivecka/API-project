d23e5f267b27810f4c3b604b71f5782ba2c46f3e
Klivecka <alexanderklivecka@ALEXANDERs-MacBook-Pro.local>
2022-09-17 10:32:27 -0600

    user login done

---
+++ frontend/src/store/session.js
@@ -0,0 +1,54 @@
+import { csrfFetch } from './csrf';
+
+const SET_USER = 'session/setUser';
+const REMOVE_USER = 'session/removeUser';
+
+const setUser = (user) => {
+  return {
+    type: SET_USER,
+    payload: user,
+  };
+};
+
+const removeUser = () => {
+  return {
+    type: REMOVE_USER,
+  };
+};
+
+export const login = (user) => async (dispatch) => {
+    console.log('THIS IS THE LOGIN THUNK GETTING HIT')
+  const { credential, password } = user;
+  console.log('THIS IS THE USER GETTING SENT', user)
+  const response = await csrfFetch('/api/session', {
+    method: 'POST',
+    body: JSON.stringify({
+      credential,
+      password,
+    }),
+  });
+  const data = await response.json();
+  console.log('THIS IS THE RESPONSE JSON', data)
+  dispatch(setUser(data));
+  return response;
+};
+
+const initialState = { user: null };
+
+const sessionReducer = (state = initialState, action) => {
+  let newState;
+  switch (action.type) {
+    case SET_USER:
+      newState = Object.assign({}, state);
+      newState.user = action.payload;
+      return newState;
+    case REMOVE_USER:
+      newState = Object.assign({}, state);
+      newState.user = null;
+      return newState;
+    default:
+      return state;
+  }
+};
+
+export default sessionReducer;
---
+++ notes.js
@@ -0,0 +1,4 @@
+window.store.dispatch(window.sessionActions.login({
+    credential: "john.smith@gmail.com",
+    password: 'secret password'
+  }));
