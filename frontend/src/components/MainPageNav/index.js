86bf77e31ce7091529ac7e3e35f9a98742a853a4
Klivecka <alexanderklivecka@ALEXANDERs-MacBook-Pro.local>
2022-09-22 09:23:04 -0600

    group list styling

--- frontend/src/components/MainPageNav/index.js
+++ frontend/src/components/MainPageNav/index.js
@@ -1,19 +1,41 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import { useState } from "react";
 import { useSelector } from "react-redux";
-import "./mainpage.css"
+import "./mainpage.css";
 
 const MainPageNav = () => {
+    const [clickState, setClickState] = useState("");
+    console.log("THIS IS MAIN PAGE NAV CLICK STATE", clickState);
     return (
-        <div className="main-page-nav-wrapper">
-            <NavLink className="main-page-nav-link" to="/groups">
-                Groups
-            </NavLink>
-            <NavLink className="main-page-nav-link" to="/events">
-                Events
-            </NavLink>
-        </div>
+        <>
+            <div className="main-page-nav-wrapper">
+                <NavLink
+                    className="main-page-nav-link"
+                    to="/groups"
+                    setClickState={setClickState}
+                    onClick={(e) => setClickState("groups")}
+                    style={{
+                        color: clickState === "groups" ? "green" : "gray",
+                        textDecoration: clickState === "groups" ? "underline" : "none"
+                    }}
+                >
+                    Groups
+                </NavLink>
+                <NavLink
+                    className="main-page-nav-link"
+                    to="/events"
+                    onClick={(e) => setClickState("events")}
+                    style={{
+                        color: clickState === "events" ? "green" : "gray",
+                        textDecoration: clickState === "events" ? "underline" : "none"
+                    }}
+                >
+                    Events
+                </NavLink>
+            </div>
+        </>
     );
 };
 
-export default MainPageNav
+export default MainPageNav;
