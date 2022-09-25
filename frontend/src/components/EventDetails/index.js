d9a1260b57f49a213fd32716aa2d5a9159d22251
Klivecka <alexanderklivecka@ALEXANDERs-MacBook-Pro.local>
2022-09-24 23:02:49 -0600

    event detail page

--- frontend/src/components/EventDetails/index.js
+++ frontend/src/components/EventDetails/index.js
@@ -2,33 +2,78 @@ import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Redirect, useParams, NavLink } from "react-router-dom";
 import { fetchEvents } from "../../store/event";
+import { fetchOneGroup } from "../../store/group";
 import "./eventdetails.css";
 
 const EventDetails = () => {
     const dispatch = useDispatch();
-    const {eventId} = useParams();
+    const { eventId } = useParams();
     const event = useSelector((state) => state.event[eventId]);
     const [isLoaded, setIsLoaded] = useState(false);
-    console.log('THIS IS THE EVENT', event)
+    console.log("THIS IS THE EVENT", event);
+
+    const date = new Date(event.startDate);
+    const newDate = date.toString();
+    const dateParse = newDate.split(" ");
+    const hour = dateParse[4].split(":")[0];
+    const minute = dateParse[4].split(":")[0];
+    let m = hour > 12 ? "PM" : "AM";
+    const dateString = `${dateParse[0]}, ${dateParse[1]} ${dateParse[2]} · ${
+        hour % 12
+    }:${minute} ${m}`;
+    event.newDate = dateString;
+
+    const group = useSelector((state) => state.group.GroupDetails);
+    let groupType;
 
     useEffect(() => {
         dispatch(fetchEvents()).then(() => setIsLoaded(true));
+        dispatch(fetchOneGroup(event.Group.id));
     }, []);
 
     return (
         <>
             {isLoaded && (
-                <div className="outer-wrapper">
-                    <div className="event-detail-image">event image</div>
-                    <div className="event-detail-text-wrapper">
-                        <div className="event-date">{event.startDate}</div>
-                        <div className="event-title">{event.name}</div>
-                        <div className="event-group">{event.Group.name}</div>
-                        <div className="event-attendance">
-                            {event.numAttending}{" "}
-                            {event.numAttending > 1 && " attendees"}
-                            {event.numAttending === 0 && " attendees"}
-                            {event.numAttending === 1 && "attendee"}
+                <div className="event-details-wrapper">
+                    <div className="outer-wrapper">
+                        <div
+                            className="event-detail-image"
+                            style={{
+                                backgroundImage: `url(${event.previewImage})`,
+                            }}
+                        ></div>
+                        <div className="event-detail-text-wrapper">
+                            <div className="event-detail-title">
+                                {event.name}
+                            </div>
+
+                            <div className="event-detail-attendance">
+                                {event.numAttending}{" "}
+                                {event.numAttending > 1 && " attendees"}
+                                {event.numAttending === 0 && " attendees"}
+                                {event.numAttending === 1 && "attendee"}
+                            </div>
+                        </div>
+                        <div className="mid-right-wrapper">
+                            <div className="event-detail-group">
+                                <div
+                                    className="tiny-group-img"
+                                    style={{
+                                        backgroundImage: group.GroupImages
+                                            .length
+                                            ? `url(${group.GroupImages[0].url})`
+                                            : `url("https://i.ibb.co/4tMJkBY/group-default.png")`,
+                                    }}
+                                ></div>
+                                <div className="event-group-details">
+                                    {group.name} <br></br>
+                                    {group.private && "Private group"}
+                                    {!group.private && "Public group"}
+                                </div>
+                            </div>
+                            <div className="event-detail-date">
+                                {event.newDate}
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -37,5 +82,4 @@ const EventDetails = () => {
     );
 };
 
-
-export default EventDetails
+export default EventDetails;
