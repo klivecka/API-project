2bceb1d9ab1a54805914d930497bedd4cbdcb0b9
Klivecka <alexanderklivecka@ALEXANDERs-MacBook-Pro.local>
2022-09-24 21:50:46 -0600

    group detail styling

--- frontend/src/components/GroupDetails/index.js
+++ frontend/src/components/GroupDetails/index.js
@@ -1,6 +1,12 @@
 import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { Redirect, useParams, NavLink, Link, useHistory } from "react-router-dom";
+import {
+    Redirect,
+    useParams,
+    NavLink,
+    Link,
+    useHistory,
+} from "react-router-dom";
 import { deleteGroup, fetchGroups, fetchOneGroup } from "../../store/group";
 import "./groupdetails.css";
 import { EventList } from "../EventList";
@@ -19,109 +25,150 @@ export const GroupDetails = () => {
     }, [fetchOneGroup]);
 
     const deleteSubmit = (groupId) => {
-
-        dispatch(deleteGroup(groupId))
-        alert("Successfully Deleted")
-        history.push('/groups')
-    }
+        dispatch(deleteGroup(groupId));
+        alert("Successfully Deleted");
+        history.push("/groups");
+    };
 
     return (
         <>
-            {isLoaded && (
-                <div className="outer-wrapper">
-                    <div
-                        className="group-detail-image"
-                        style={{
-                            backgroundImage: group.GroupImages.length
-                                ? `url(${group.GroupImages[0].url})`
-                                : `url("https://i.ibb.co/4tMJkBY/group-default.png")`,
-                        }}
-                    ></div>
-                    <div className="group-detail-text-wrapper">
-                        <div className="group-detail-title">{group.name}</div>
-                        <div className="group-detail-location">
-                            <i className="fa-solid fa-location-dot"></i>
-                            {"    "}
-                            {group.city}, {group.state}{" "}
-                        </div>
-                        <div className="group-detail-members">
-                            <i className="fa-solid fa-users"></i>
-                            {"    "}
-                            {group.numMembers}
-                            {group.numMembers > 1 && " members"}
-                            {group.numMembers === 0 && " members"}
-                            {group.numMembers === 1 && " member"} ·{" "}
-                            {group.private === true && "Private group"}
-                            {group.private === false && "Public group"}
-                        </div>
-                        <div className="group-details-organized">
-                            <i className="fa-solid fa-user"></i>
-                            {"    "}
-                            Organized by {group.Organizer.firstName}{" "}
-                            {group.Organizer.lastName}
-                        </div>
-                        {userId === group.organizerId && (
-                            <>
-                            <div className="join-group-div">
-                                <Link to={`/groups/edit/${groupId}`}>
-                                    <button id="function-button">
-                                        Edit this group
-                                    </button>
-                                </Link>
+            <div className="group-details-wrapper">
+                {isLoaded && (
+                    <div className="outer-wrapper">
+                        <div
+                            className="group-detail-image"
+                            style={{
+                                backgroundImage: group.GroupImages.length
+                                    ? `url(${group.GroupImages[0].url})`
+                                    : `url("https://i.ibb.co/4tMJkBY/group-default.png")`,
+                            }}
+                        ></div>
+                        <div className="group-detail-text-wrapper">
+                            <div className="group-detail-title">
+                                {group.name}
                             </div>
-                            <div className="create-event-div">
-                            <Link to={`/groups/${groupId}/event`}>
-                                    <button id="function-button">
-                                        Add an event
-                                    </button>
-                                </Link>
+                            <div className="group-detail-location">
+                                <i className="fa-solid fa-location-dot"></i>
+                                {"    "}
+                                {group.city}, {group.state}{" "}
                             </div>
-                            <div className="delete-group-div">
-                            
-                                    <button id="function-button"
-                                    onClick={e => deleteSubmit(groupId)}
-                                    >
-                                        Delete this group
-                                    </button>
-                            
+                            <div className="group-detail-members">
+                                <i className="fa-solid fa-users"></i>
+                                {"    "}
+                                {group.numMembers}
+                                {group.numMembers > 1 && " members"}
+                                {group.numMembers === 0 && " members"}
+                                {group.numMembers === 1 && " member"} ·{" "}
+                                {group.private === true && "Private group"}
+                                {group.private === false && "Public group"}
                             </div>
-                            </>
+                            <div className="group-details-organized">
+                                <i className="fa-solid fa-user"></i>
+                                {"    "}
+                                Organized by {group.Organizer.firstName}{" "}
+                                {group.Organizer.lastName}
+                            </div>
+                            {userId === group.organizerId && (
+                                <>
+                                    <div className="join-group-div">
+                                        <Link to={`/groups/edit/${groupId}`}>
+                                            <button id="function-button">
+                                                Edit this group
+                                            </button>
+                                        </Link>
+                                    </div>
+                                    <div className="create-event-div">
+                                        <Link to={`/groups/${groupId}/event`}>
+                                            <button id="function-button">
+                                                Add an event
+                                            </button>
+                                        </Link>
+                                    </div>
+                                    <div className="delete-group-div">
+                                        <button
+                                            id="function-button"
+                                            onClick={(e) =>
+                                                deleteSubmit(groupId)
+                                            }
+                                        >
+                                            Delete this group
+                                        </button>
+                                    </div>
+                                </>
+                            )}
+                        </div>
+                        <div className="group-details-nav-bar">
+                            <div
+                                className="detail-nav-link"
+                                onClick={(e) => setLinkValue("about")}
+                                style={{
+                                    color:
+                                        linkValue === "about"
+                                            ? "#0b7880"
+                                            : "black",
+                                    fontWeight:
+                                        linkValue === "about" ? "bold" : "400",
+                                }}
+                            >
+                                About
+                            </div>
+                            <div
+                                className="detail-nav-link"
+                                onClick={(e) => setLinkValue("events")}
+                                style={{
+                                    color:
+                                        linkValue === "events"
+                                            ? "#0b7880"
+                                            : "black",
+                                    fontWeight:
+                                        linkValue === "events" ? "bold" : "400",
+                                }}
+                            >
+                                Events
+                            </div>
+                            {/* <div
+                                className="detail-nav-link"
+                                onClick={(e) => setLinkValue("members")}
+                                style={{
+                                    color:
+                                        linkValue === "members"
+                                            ? "#0b7880"
+                                            : "black",
+                                    fontWeight:
+                                        linkValue === "members"
+                                            ? "bold"
+                                            : "400",
+                                }}
+                            >
+                                Members
+                            </div> */}
+                            {/* <div
+                                className="detail-nav-link"
+                                onClick={(e) => setLinkValue("photos")}
+                                style={{
+                                    color:
+                                        linkValue === "photos"
+                                            ? "#0b7880"
+                                            : "black",
+                                    fontWeight:
+                                        linkValue === "photos" ? "bold" : "400",
+                                }}
+                            >
+                                Photos
+                            </div> */}
+                        </div>
+                        {linkValue === "about" && (
+                            <h2 className="about-text">What we're about</h2>
                         )}
-                    </div>
-                    <div className="group-details-nav-bar">
-                        <div
-                            className="detail-nav-link"
-                            onClick={(e) => setLinkValue("about")}
-                        >
-                            About
-                        </div>
-                        <div
-                            className="detail-nav-link"
-                            onClick={(e) => setLinkValue("events")}
-                        >
-                            Events
-                        </div>
-                        <div
-                            className="detail-nav-link"
-                            onClick={(e) => setLinkValue("members")}
-                        >
-                            Members
-                        </div>
-                        <div
-                            className="detail-nav-link"
-                            onClick={(e) => setLinkValue("photos")}
-                        >
-                            Photos
+                        <div className="group-details-nav-click">
+                            {linkValue === "about" && group.about}
+                            {linkValue === "events" && <EventList />}
+                            {linkValue === "members" && "members TBD"}
+                            {linkValue === "photos" && group.GroupImages[0].url}
                         </div>
                     </div>
-                    <div className="group-details-nav-click">
-                        {linkValue === "about" && group.about}
-                        {linkValue === "events" && <EventList />}
-                        {linkValue === "members" && "members TBD"}
-                        {linkValue === "photos" && group.GroupImages[0].url}
-                    </div>
-                </div>
-            )}
+                )}
+            </div>
         </>
     );
 };
